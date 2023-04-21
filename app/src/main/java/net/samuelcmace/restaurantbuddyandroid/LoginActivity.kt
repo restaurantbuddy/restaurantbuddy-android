b05ebb0565ee98e3d04e251bfff9325bf0bec590
Samuel Mace <samuelcmace@gmail.com>
2023-04-21 15:34:34 -0500

    Add Additional Activities, Update Documentation, and Update Gitignore

--- app/src/main/java/net/samuelcmace/restaurantbuddyandroid/LoginActivity.kt
+++ app/src/main/java/net/samuelcmace/restaurantbuddyandroid/LoginActivity.kt
@@ -3,11 +3,9 @@ package net.samuelcmace.restaurantbuddyandroid
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
-
 }
